19682db91ee9864d487cea560dcf1d47228f0744
Samily Suelen <samily.ennet@gmail.com>
2022-07-15 14:23:44 +0000

    Agregação

--- Colecoes/consultas.py
+++ Colecoes/consultas.py
@@ -1,7 +1,32 @@
 import pymongo
 myclient = pymongo.MongoClient("mongodb+srv://bd2:bd2senha@cluster0.lwjkt.mongodb.net/?retryWrites=true&w=majority")
 mydb = myclient["petshop"]
-mycol = mydb["pet"]
+mycol_pet = mydb["pet"] 
 
-myquery = { "nome": "Dory" }
-mydoc = mycol.find(myquery)
+
+#myquery = { "nome": "Dory" }
+#mydoc = mycol.find(myquery)
+
+#for x in mydoc:
+  #print(x)
+
+
+
+
+result = mycol_pet.aggregate([
+    {
+        '$lookup': {
+            'from': 'tutor',
+            'localField': 'ID_Tutor',
+            'foreignField': 'id',
+            'as': 'joinedResult'
+        }
+    }
+])
+
+myquery = { "nome": { "$regex": "^S" } }
+
+mydoc = mycol_pet.find(myquery)
+
+for x in mydoc:
+  print(x)
--- Colecoes/principal.py
+++ Colecoes/principal.py
@@ -1,1 +1,7 @@
 print("Olá ")
+
+#- Consulta por parte do nome x
+#- Parte interativa do menu com if
+#- Consulta por duas coleções (Retornar dados de mais de uma coleção) 
+#- Ajustar ID tutor
+#- Funções para inserir e consultar dados das duas coleçõessa x

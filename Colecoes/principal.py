80bf9fc15d69b8d04a7c1daee054c81f28b0a458
Samily Suelen <samily.ennet@gmail.com>
2022-07-15 14:29:31 +0000

    atu

--- Colecoes/principal.py
+++ Colecoes/principal.py
@@ -3,5 +3,5 @@ print("Olá ")
 #- Consulta por parte do nome x
 #- Parte interativa do menu com if
 #- Consulta por duas coleções (Retornar dados de mais de uma coleção) 
-#- Ajustar ID tutor
+#- Ajustar ID tutor x
 #- Funções para inserir e consultar dados das duas coleçõessa x
